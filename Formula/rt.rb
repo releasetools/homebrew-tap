76b27b29409b34dc34bfbfb7ff6d2d2579353460
Mihai Bojin <mihai.bojin@gmail.com>
2024-12-03 00:56:34 +0000

    fix sha for 0.0.9

--- Formula/rt.rb
+++ Formula/rt.rb
@@ -2,7 +2,7 @@ class Rt < Formula
   desc "Release tools for shell and GitHub workflows"
   homepage "https://release.tools"
   url "https://github.com/releasetools/bash/releases/download/v0.0.9/releasetools.bash"
-  sha256 "4b4040dd71db5c1db4d780ffa691fe33d0d35f0196644c1d78492ac771d74aa2"
+  sha256 "e14b6ce05701ac1d56fecc84b7b27c3099d2d8f334ceb5a75784264497ca9f31"
   license "Apache-2.0"
   head "https://github.com/releasetools/bash.git", branch: "main"
 
